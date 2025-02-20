ad1e1e2d8ccdb00c14625dddcdcae1c97cb226ff
Vedang <vedang.wappnet@gmail.com>
2025-02-20 12:49:59 +0530

    authRoute is created

---
+++ backend/api_gateway/routes/authRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { services } = require("../config/service");
+const router = express.Router();
+
+router.use(
+    "/",
+    createProxyMiddleware({
+        target: services.auth,
+        changeOrigin: true,
+        onProxyReq: (proxyReq, req, res) => {
+            console.log(`Proxying request to backend at port 8002`);
+        },
+    })
+);
+
+module.exports = router;
